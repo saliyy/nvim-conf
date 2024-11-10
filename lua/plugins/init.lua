4a63f95e58a5fdb4a5995afe815f61eea888330c
Vladislav Saly <vladislav.saly@lamoda.ru>
2024-11-10 20:25:25 +0300

    config

---
+++ lua/plugins/init.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "stevearc/dressing.nvim",
+    lazy = false,
+    opts = {}
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      "lua-language-server",
+      "stylua",
+      "prettier",
+      "gopls",
+    }
+  },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "javascript", "typescript", "go"
+  		},
+  	},
+  },
+}
